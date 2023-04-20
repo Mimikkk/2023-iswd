d83526e2dc023b34b452cb1b8c92faacbf23c9d9
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-04-20 21:44:59 +0200

    validity

--- project-2-cheater/src/mod/players/danielos.py
+++ project-2-cheater/src/mod/players/danielos.py
@@ -8,14 +8,6 @@ from .extended_player import ExtendedPlayer
 Card = tuple[int, int]
 
 
-def increase_by(card: Card, declarable: list[Card], value: int):
-  if value == 0: return card, card
-
-  declarable = [c for c in declarable if c[0] == card[0] + value]
-  if not declarable: return increase_by(card, declarable, value - 1)
-  return card, choice(declarable)
-
-
 class DanielosPlayer(ExtendedPlayer):
   Cards = tuple(
     (rank, color)
@@ -23,23 +15,24 @@ class DanielosPlayer(ExtendedPlayer):
     for color in range(4)
   )
 
+  def __init__(self, name):
+    super().__init__(name)
+
   def declare(self, declared):
     valid = [card for card in self.cards if self.is_valid(card, declared)]
-    declarable = [card for card in self.Cards if self.is_valid(card, declared)]
+    if len(self.cards) == 1 and len(valid) == 1: return valid[0], valid[0]
     if not valid: return "draw"
 
     card = declaration = min(self.cards, key=self.by_rank)
 
     if declared and not self.is_valid(card, declared):
-      declaration = (min(declared[0] + 1, 14), declaration[1])
-      declaration = self.pick(declaration)
+      minimum = min(declared[0] + 1, 14)
+      declaration = (minimum, card[1])
+
+      viable = [card for card in self.cards if card[0] in (minimum, minimum + 1)]
+      declaration = choice(viable or [declaration])
 
     return card, declaration
 
   def should_accuse(self, declared):
     return declared in self.cards
-
-  def pick(self, declaration):
-    for card in self.cards:
-      if card[0] in (declaration[0], declaration[0] + 1): return card
-    return declaration
