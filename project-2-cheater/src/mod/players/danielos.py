25c6e99b3ea857319d70e36f7433cf24bb2d6459
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-04-20 21:33:50 +0200

    validity

--- project-2-cheater/src/mod/players/danielos.py
+++ project-2-cheater/src/mod/players/danielos.py
@@ -23,44 +23,17 @@ class DanielosPlayer(ExtendedPlayer):
     for color in range(4)
   )
 
-  def __init__(self, name: str):
-    super().__init__(name)
-    self.suspected = set()
-    self.pile = []
-
-  def on_right_accusation(self, revealed, taken_count):
-    self.on_draw()
-
-  def on_wrong_accusation(self, revealed, taken_count):
-    self.suspected.remove(revealed)
-    self.on_draw()
-
-  def on_caught(self, taken_count):
-    self.on_draw()
-
-  def on_honest(self, taken_count):
-    self.on_draw()
-
-  def on_opponent_draw(self):
-    self.on_draw()
-
-  def on_start(self):
-    for card in self.cards: self.suspected.add(card)
-
-  def on_take(self, taken):
-    for card in taken: self.suspected.add(card)
-
   def declare(self, declared):
     valid = [card for card in self.cards if self.is_valid(card, declared)]
+    declarable = [card for card in self.Cards if self.is_valid(card, declared)]
     if not valid: return "draw"
 
     card = declaration = min(self.cards, key=self.by_rank)
 
-    if declared:
-      minimum = declared[0]
-      if card[0] < minimum:
-        declaration = (min(minimum + 1, 14), declaration[1])
-        declaration = self.pick(declaration)
+    if declared and not self.is_valid(card, declared):
+      declaration = (min(declared[0] + 1, 14), declaration[1])
+      declaration = self.pick(declaration)
+
     return card, declaration
 
   def should_accuse(self, declared):
@@ -70,17 +43,3 @@ class DanielosPlayer(ExtendedPlayer):
     for card in self.cards:
       if card[0] in (declaration[0], declaration[0] + 1): return card
     return declaration
-
-  def present_state(self, declared):
-    print(declared)
-    print(sorted(self.cards))
-    print(sorted(self.suspected))
-    print(self.pile)
-    print(self.state.pile)
-    print(sorted(self.state.players[0].cards))
-    print(sorted(self.state.players[1].cards))
-    print()
-
-  def on_draw(self):
-    for _ in range(3):
-      if self.pile and (card := self.pile.pop()) in self.suspected: self.suspected.remove(card)
